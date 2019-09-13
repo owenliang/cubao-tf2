d3fd4d3723b0b46988d1b4ff1b6bed55d5fcdf11
owenliang <owenliang1990@gmail.com>
2019-09-13 21:05:18 +0800

    update

---
+++ image.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 19:44:19 2019
+
+@author: liangdong
+"""
+
+import numpy as np
+
+image = np.array([[
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 2, 1, 0],
+    [0, 0, 2, 2, 0, 1, 0],
+    [0, 1, 1, 0, 2, 1, 0],
+    [0, 0, 2, 1, 1, 0, 0],
+    [0, 2, 1, 1, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]], dtype=np.float32)
+
+image = np.expand_dims(image, axis=-1) 
+print(image)
